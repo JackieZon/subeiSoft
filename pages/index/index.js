c1021aeffa9c2ff5e079ee3f9b0a3419ad15ff1d
JackieZon <mrjackie168@gmail.com>
2017-06-29 19:06:39 +0800

    2017-06-29 19:06

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,68 @@
 //index.js
 //获取应用实例
-var app = getApp()
+var app = getApp();
 Page({
   data: {
     motto: '速贝 - 软件',
-    userInfo: {}
+    userInfo: {},
+    tempFilePath: '',
+    savedFilePath: wx.getStorageSync('savedFilePath') || '',
+    dialog: {
+      hidden: true
+    }
+  },
+  chooseImage: function () {
+    var that = this
+    wx.chooseImage({
+      count: 1,
+      success: function (res) {
+        that.setData({
+          tempFilePath: res.tempFilePaths[0]
+        })
+      }
+    })
+  },
+  saveFile: function () {
+    if (this.data.tempFilePath.length > 0) {
+      var that = this
+      wx.saveFile({
+        tempFilePath: this.data.tempFilePath,
+        success: function (res) {
+          that.setData({
+            savedFilePath: res.savedFilePath
+          })
+          wx.setStorageSync('savedFilePath', res.savedFilePath)
+          that.setData({
+            dialog: {
+              title: '保存成功',
+              content: '下次进入应用时，此文件仍可用',
+              hidden: false
+            }
+          })
+        },
+        fail: function (res) {
+          that.setData({
+            dialog: {
+              title: '保存失败',
+              content: '应该是有 bug 吧',
+              hidden: false
+            }
+          })
+        }
+      })
+    }
+  },
+  clear: function () {
+    wx.setStorageSync('savedFilePath', '')
+    this.setData({
+      tempFilePath: '',
+      savedFilePath: ''
+    })
+  },
+  confirm: function () {
+    this.setData({
+      'dialog.hidden': true
+    })
   },
   //事件处理函数
   bindViewTap: function() {
@@ -13,7 +71,8 @@ Page({
     })
   },
   onLoad: function () {
-    console.log('onLoad')
+    console.log('onLoad');
+    console.log(this.data);
     var that = this
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
@@ -22,5 +81,77 @@ Page({
         userInfo:userInfo
       })
     })
+  },
+  onPullDownRefresh(){
+    console.log('我正在刷新页面');
+    setTimeout(function(){
+      wx.stopPullDownRefresh();
+    },1000)
+  },
+  onShareAppMessage(){
+    return {
+      title: '我分享的是打印项目(小程序)',
+      path: 'pages/index/index'
+    }
+  },
+  chooseFile: function(){
+    wx.chooseImage({
+      success: function(res) {
+        var tempFilePaths = res.tempFilePaths
+        console.log(tempFilePaths);
+      }
+    })
+    // wx.downloadFile({
+    //   url: 'http://example.com/somefile.pdf',
+    //   success: function (res) {
+    //     var filePath = res.tempFilePath
+    //     wx.openDocument({
+    //       filePath: filePath,
+    //       success: function (res) {
+    //         console.log('打开文档成功')
+    //       }
+    //     })
+    //   }
+    // })
+  },
+  navigateTo: function () {
+    wx.navigateTo({ url: './navigator' })
+  },
+  onclick(){
+    // wx.showModal({
+    //   title: '提示',
+    //   content: '这是一个模态弹窗',
+    //   success: function(res) {
+    //     if (res.confirm) {
+    //       console.log('用户点击确定')
+    //     } else if (res.cancel) {
+    //       console.log('用户点击取消')
+    //     }
+    //   }
+    // })
+    // wx.showActionSheet({
+    //   itemList: ['A', 'B', 'C'],
+    //   success: function(res) {
+    //     console.log(res.tapIndex)
+    //   },
+    //   fail: function(res) {
+    //     console.log(res.errMsg)
+    //   }
+    // })
+    // wx.showNavigationBarLoading();
+    // wx.chooseAddress({
+    //   success: function (res) {
+    //     console.log(res.userName)
+    //     console.log(res.postalCode)
+    //     console.log(res.provinceName)
+    //     console.log(res.cityName)
+    //     console.log(res.countyName)
+    //     console.log(res.detailInfo)
+    //     console.log(res.nationalCode)
+    //     console.log(res.telNumber)
+    //   }
+    // })
+
+    console.log('on click');
   }
 })
--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -4,11 +4,21 @@ Page({
   data: {
     logs: []
   },
+  goPage(){
+    wx.navigateTo({
+      url: './../api/page-chenge/new-page'
+    })
+  },
   onLoad: function () {
+    // this.setData({
+    //   logs: (wx.getStorageSync('logs') || []).map(function (log) {
+    //     return util.formatTime(new Date(log))
+    //   })
+    // })
     this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(function (log) {
-        return util.formatTime(new Date(log))
-      })
+      logs: [
+        '日志内容S'
+      ]
     })
   }
 })
